760bc82c8061dd38ca803bf7e74067b25480cb5b
Rohit Keshwani <rohit.keshwani@razorpay.com>
2021-08-11 15:10:24 +0530

    test with length cal

--- x25519_test.go
+++ x25519_test.go
@@ -1,11 +1,12 @@
 package cryptobench
 
 import (
+	"fmt"
 	"testing"
 )
 
 func BenchmarkEncryptOAEP(b *testing.B) {
-	msg := `Invalid string`
+	msg := `merchant_id123:t:1628674459`
 
 	for n := 0; n < b.N; n++ {
 		EncryptOAEP(msg)
@@ -13,7 +14,7 @@ func BenchmarkEncryptOAEP(b *testing.B) {
 }
 
 func BenchmarkDecryptOAEP(b *testing.B) {
-	msg := `Invalid string`
+	msg := `merchant_id123:t:1628674459`
 	enMsg, _ := EncryptOAEP(msg)
 
 	for n := 0; n < b.N; n++ {
@@ -22,7 +23,7 @@ func BenchmarkDecryptOAEP(b *testing.B) {
 }
 
 func BenchmarkEncryptPKCS1v15(b *testing.B) {
-	msg := `Invalid string`
+	msg := `merchant_id123:t:1628674459`
 
 	for n := 0; n < b.N; n++ {
 		EncryptPKCS1v15(msg)
@@ -30,10 +31,17 @@ func BenchmarkEncryptPKCS1v15(b *testing.B) {
 }
 
 func BenchmarkDecryptPKCS1v15(b *testing.B) {
-	msg := `Invalid string`
+	msg := `merchant_id123:t:1628674459`
 	enMsg, _ := EncryptPKCS1v15(msg)
 
 	for n := 0; n < b.N; n++ {
 		DecryptPKCS1v15(enMsg)
 	}
+}
+
+func TestEncryptOAEP(b *testing.T) {
+	msg := `0123456789123:t:1628674459`
+
+	enMsg, _ :=	EncryptOAEP(msg)
+	fmt.Println(len(enMsg))
 }
